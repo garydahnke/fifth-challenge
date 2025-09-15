084d560876c6fd4852ab27f8fdcf9fec1c873c6a
garydahnke <126987955+garydahnke@users.noreply.github.com>
2025-09-14 21:58:49 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "ec2" {
     ami = "ami-0bb84b8ffd87024d8"
     instance_type = "t2.micro"
-    tags = {
-        Name = "my_ec2"
-    }
+#    tags = {
+#        Name = "my_ec2"
+#    }
 }
